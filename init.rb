349d98240ecd94f694535ee5751f09ab8d1abdf8
vincentsys <p.probst@vincentsystems.de>
2016-03-29 09:52:41 +0200

    Settings to enable module per project
    Added option to enable module for some projects only in project modules
    settings. Was enabled for all projects before.

--- init.rb
+++ init.rb
@@ -4,9 +4,11 @@ Redmine::Plugin.register :sphinx do
   name 'Redmine Sphinx plugin'
   author 'vincentsys'
   description 'Sphinx docu plugin for redmine'
-  version '0.1.0'
+  version '0.2.0'
 
   # Add sphinx document tab
-  permission :sphinx, {:sphinx => [:index]}, :public => true
+  project_module :sphinx do
+    permission :sphinx, {:sphinx => [:index]}, :public => true
+  end
   menu :project_menu, :sphinx_list, { :controller => 'sphinx', :action => 'index' }, :caption => 'Sphinx Documents', :param => :project_id
 end
